9d9f0c8ca8d9a0fae6f3f89a62324b19e52b902f
Dean Abner Julian <dabnerjulian@yahoo.com>
2024-08-16 11:55:12 +0700

    refactor: Update navigation link in nav.blade.php

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -12,7 +12,7 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link fw-bold" aria-current="page" href="#services">OUR SERVICES</a>
+                    <a class="nav-link fw-bold" aria-current="page" href="#what-we-do">OUR SERVICES</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link fw-bold" href="#how-we-do-it">HOW WE DO IT</a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,18 @@
 <!doctype html>
 <html lang="en">
   <head>
+    {{-- Gtag --}}
+    @if (config('app.env') === 'production')
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-CXH0CPZB18"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-CXH0CPZB18');
+    </script>
+    @endif
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'Welcome') | {{ appName() }}</title>
@@ -19,20 +31,8 @@
     {{-- vite --}}
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-    {{-- Tag Manager --}}
-    @if (config('app.env') === 'production')
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T22ZX468');</script>
-    <!-- End Google Tag Manager -->
-    @endif
   </head>
   <body>
-
-    @if (config('app.env') === 'production')
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T22ZX468"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    @endif
 
     @include('includes.nav')
 
