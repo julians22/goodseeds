1ab16fadddbb04e3c36e46c3ae4dc98f4b760b76
Dean Abner Julian <dabnerjulian@yahoo.com>
2024-08-16 10:43:14 +0700

    refactor: Update image paths for carousel and icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,7 @@
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T22ZX468"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
+    @endif
 
     @include('includes.nav')
 
