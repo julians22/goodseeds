cbc319c4792354fec78f80e7a2ce260c6056a354
Dean Abner Julian <dabnerjulian@yahoo.com>
2024-08-31 15:25:17 +0700

    refactor: Update general.notificationRecipients to an empty array in the database settings migration

--- database/settings/2024_08_28_041947_update_general_settings.php
+++ database/settings/2024_08_28_041947_update_general_settings.php
@@ -6,6 +6,6 @@ return new class extends SettingsMigration
 {
     public function up(): void
     {
-        $this->migrator->add('general.notificationRecipients', '');
+        $this->migrator->add('general.notificationRecipients', []);
     }
 };
